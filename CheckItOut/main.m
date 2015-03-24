231dd4c1551cefabd68ce7cec841b98de6097412
nielubowicz <nielubowicz@gmail.com>
2015-03-23 22:18:04 -0400

    Initial Project Commit

---
+++ CheckItOut/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CheckItOut
+//
+//  Created by chris nielubowicz on 3/23/15.
+//  Copyright (c) 2015 Assorted Intelligence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ CheckItOutTests/CheckItOutTests.m
@@ -0,0 +1,34 @@
+//
+//  CheckItOutTests.m
+//  CheckItOutTests
+//
+//  Created by chris nielubowicz on 3/23/15.
+//  Copyright (c) 2015 Assorted Intelligence. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CheckItOutTests : XCTestCase
+
+@end
+
+@implementation CheckItOutTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
