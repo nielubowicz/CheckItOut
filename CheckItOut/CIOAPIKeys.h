e1444de4b51812bd470cd7ba7aeff7f380ab7fad
Chris Nielubowicz <cnielubowicz@mobiquityinc.com>
2015-03-24 09:47:14 -0400

    Updated APIKeys -- use .m/.h instead of PList.

---
+++ CheckItOut/CIOAPIKeys.h
@@ -0,0 +1,12 @@
+//
+//  CIOAPIKeys.h
+//  CheckItOut
+//
+//  Created by Chris Nielubowicz on 3/24/15.
+//  Copyright (c) 2015 Assorted Intelligence. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT NSString *const ParseApplicationIdentifier;
+FOUNDATION_EXPORT NSString *const ParseRESTAPIKey;
